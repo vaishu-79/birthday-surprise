2d6264d476413936a330821e15ea29c6375c08ec
vaishu-79 <kvaishnavi635@gmail.com>
2026-07-10 11:50:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+// Continue button
+function showLogin() {
+
+    document.getElementById("warning-page").style.display = "none";
+
+    document.getElementById("login-page").style.display = "block";
+
+}
+
+
+// Login button
+function login() {
+
+    let username = document.getElementById("username").value;
+
+    let password = document.getElementById("password").value;
+
+
+    if(username === "SAROVAISHU" && password === "2307"){
+
+        alert("Welcome My Love ❤️");
+
+        document.body.innerHTML = `
+
+        <div style="
+        height:100vh;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        flex-direction:column;
+        background:linear-gradient(135deg,#ff9a9e,#fad0c4);
+        ">
+
+        <h1 style="
+        font-size:60px;
+        color:#ff0066;
+        ">
+        🎉 HAPPY BIRTHDAY SARO ❤️
+        </h1>
+
+        <h2 style="
+        color:white;
+        margin-top:20px;
+        ">
+        Made with ❤️ by Vaishu
+        </h2>
+
+        </div>
+
+        `;
+
+    }
+
+    else{
+
+        alert("Wrong Username or Password 😢");
+
+    }
+
+}
