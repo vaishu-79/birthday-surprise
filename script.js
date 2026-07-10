614df72668de2de754dd910105959fac4239e7b0
vaishu-79 <kvaishnavi635@gmail.com>
2026-07-10 13:15:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,40 +15,44 @@ function login() {
 
     let password = document.getElementById("password").value.trim();
 
+
     if(username === "SAROVAISHU" && password === "2307"){
 
         alert("Welcome My Love ❤️");
 
+
         document.body.innerHTML = `
 
-        <div style="
-        height:100vh;
-        display:flex;
-        justify-content:center;
-        align-items:center;
-        flex-direction:column;
-        background:linear-gradient(135deg,#ff9a9e,#fad0c4);
-        ">
+        <div class="birthday-page">
 
-        <h1 style="
-        font-size:60px;
-        color:#ff0066;
-        ">
-        🎉 HAPPY BIRTHDAY SARO ❤️
-        </h1>
+            <h1>
+            🎉 HAPPY BIRTHDAY SARO ❤️
+            </h1>
 
-        <h2 style="
-        color:white;
-        margin-top:20px;
-        ">
-        Made with ❤️ by Vaishu
-        </h2>
+
+            <img src="saro.jpg" class="photo">
+
+
+            <p>
+            You are the most special person in my life ❤️
+            <br>
+            Thank you for bringing happiness into my world.
+            <br>
+            Always stay happy and keep smiling 😊
+            </p>
+
+
+            <h2>
+            Made with ❤️ by Vaishu
+            </h2>
+
 
         </div>
 
         `;
 
     }
+
 
     else{
 
