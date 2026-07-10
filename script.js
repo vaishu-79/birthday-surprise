4fde279e1afcc3dfab60dbecb65d0a904f5eddac
vaishu-79 <kvaishnavi635@gmail.com>
2026-07-10 12:05:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function login() {
     let password = document.getElementById("password").value;
 
 
-    if(username === "SAROVAISHU" && password === "2307"){
+    if(username === "SAROVAISHU" && password === "2379"){
 
         alert("Welcome My Love ❤️");
 
