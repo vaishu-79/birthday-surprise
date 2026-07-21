d6e4946f80c3d29d338f88325cbb75c2d976f51c
vaishu-79 <kvaishnavi635@gmail.com>
2026-07-21 21:58:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -662,3 +662,23 @@ fire.remove();
 
 
 }
+function updateClock(){
+
+    const now = new Date();
+
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+
+    h = String(h).padStart(2,"0");
+    m = String(m).padStart(2,"0");
+    s = String(s).padStart(2,"0");
+
+    document.getElementById("clock").innerHTML =
+    "🕒 " + h + ":" + m + ":" + s;
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
